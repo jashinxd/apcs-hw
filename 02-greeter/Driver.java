266ff3e6fc4852e12468e854b51504bd3ac946da
Jason Shin <jasonshin98@gmail.com>
2014-09-15 08:42:11 -0400

    Added

---
+++ 02-greeter/Driver.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	Greeter g;
+	g = new Greeter();
+	System.out.println(g);
+	g.greet();
+	g.greet();
+	
+    }
+}
--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -6,5 +6,10 @@ import java.util.*;
 public class Greeter {
     public void greet() {
 	System.out.println("Hello World!");
+	ungreet();
+    }
+    
+    private void ungreet() {
+	System.out.println("I'm out");
     }
 }
