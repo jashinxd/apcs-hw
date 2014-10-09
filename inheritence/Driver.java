fc2730a46151117110070cd5e0b9707dfabdc806
Jason Shin <jasonshin98@gmail.com>
2014-10-09 08:01:26 -0400

    Added Hw 10

--- inheritence/Driver.java
+++ inheritence/Driver.java
@@ -1,19 +1,26 @@
 public class Driver {
 
     public static void main(String[] args){
-	Basechar c = new Basechar(),c2;
-	Warrior w = new Warrior(),w2;
-	Mage m = new Mage(),m2;
+	Basechar c = new Basechar("Hello"),c2;
+	//Warrior w = new Warrior("Groo"),w2;
+	Mage mdef = new Mage();
+	Mage mnam = new Mage("Mr. Sage"),m2;
+	Mage mboth = new Mage("Mr. Page", 300);
 
-	w.setName("Groo");
-	m.setName("Mr. Sage");
+	//w.setName("Groo");
+	//m.setName("Mr. Sage");
 
-	w.attack(m);
-	m.attack(w);
-	w.attack(w);
+	//w.attack(m);
+	//m.attack(w);
+	//w.attack(w);
 
-	System.out.println(w.toString());
-	System.out.println(w);
+	//System.out.println(w.toString());
+	//System.out.println(w);
+	
+	//System.out.println(m.getName());
+	System.out.println(mboth.getManna());
+	System.out.println(mboth.getName());
+
     }
 
 
--- inheritence/Mage.java
+++ inheritence/Mage.java
@@ -2,7 +2,23 @@
 public class Mage extends Basechar{
     private int manna = 200;
 
+    public Mage (){
+    }
+
+    public Mage(String n){
+	setName(n);
+    }
+
+    public Mage(String n, int m){
+	setName(n);
+	setManna(m);
+    }
+
     public int getManna(){
 	return manna;
+    }
+
+    public void setManna(int m){
+	manna = m;
     }
 }
