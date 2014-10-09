eb3371f5672c1cd73ed2259e8a6641defc2ba318
Jason Shin <jasonshin98@gmail.com>
2014-10-08 20:02:59 -0400

    Added HW 10

--- inheritence/Driver.java
+++ inheritence/Driver.java
@@ -1,15 +1,19 @@
 public class Driver {
 
     public static void main(String[] args){
-	Basechar c = new Basechar();
-	Warrior w = new Warrior();
-	Mage m = new Mage();
-	System.out.println(c.getHealth());
-	System.out.println(w.getHealth());
-	System.out.println(m.getHealth());
+	Basechar c = new Basechar(),c2;
+	Warrior w = new Warrior(),w2;
+	Mage m = new Mage(),m2;
 
-	System.out.println(m.getManna());
+	w.setName("Groo");
+	m.setName("Mr. Sage");
 
+	w.attack(m);
+	m.attack(w);
+	w.attack(w);
+
+	System.out.println(w.toString());
+	System.out.println(w);
     }
 
 
