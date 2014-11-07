1ca1cf438db7b4667249a2757fe969fb9b46b761
Jason Shin <jasonshin98@gmail.com>
2014-11-07 08:39:39 -0500

    Added 15-sarray

--- 15-sarray/sarray.java
+++ 15-sarray/sarray.java
@@ -1,21 +1,34 @@
 public class sarray {
-    public int[] data;
+    private int[] data;
+    private int nextBlank;
+    
 
-    public sarray() {
+    public sarray(int i) {
 	//Start array at size 10
+	data = new int[i];
+	nextBlack = 0;
     }
 
     public void add(int i) {
 	//Add int to the end
-	
+	while (data[nextBlank] != 0) {
+	    nextBlank++;
+	}
+	data[nextBlank] = i;
+	nextBlank++;
     }
 
     public void add(int index, int i) {
 	//Add at index, shifts everything else down
+	for (int x = data.length - 1; x > index; x = x - 1) {
+	    data[x] = data[x - 1];
+	}
+	data[index] = i
     }
 
     public void get(int index) {
         //Get item at index
+	
     }
 
     public void set(int index, int i) {
