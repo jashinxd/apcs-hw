3bcb0498625ffc69c01a906c44cbddffa32fe8b0
Jason Shin <jasonshin98@gmail.com>
2014-11-07 08:16:59 -0500

    Added sarray.java

---
+++ 14-Arraystuff/sarray.java
@@ -0,0 +1,28 @@
+public class sarray {
+    public int[] data;
+
+    public sarray() {
+	//Start array at size 10
+    }
+
+    public void add(int i) {
+	//Add int to the end
+    }
+
+    public void add(int index, int i) {
+	//Add at index, shifts everything else down
+    }
+
+    public void get(int index) {
+        //Get item at index
+    }
+
+    public void set(int index, int i) {
+	//Set item at index to int t
+    }
+
+    public void size(){
+	//return number of items in set
+    }
+
+    public 
