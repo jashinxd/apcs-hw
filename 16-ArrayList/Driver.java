4bb456e56c09e48e99f9bbf71dbfcba01e1b1e6a
Jason Shin <jasonshin98@gmail.com>
2014-11-12 08:40:48 -0500

    Added 16-ArrayList

---
+++ 16-ArrayList/Driver.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+    public static void main(String[] args){
+	ArrayList<Integer> al = new ArrayList<Integer>();
+	Random r = new Random();
+	for (int i = 0; i < 20; i++) {
+	    ai.add(r.nextInt(5));
+	}
+	for (int i = 0; i < 
