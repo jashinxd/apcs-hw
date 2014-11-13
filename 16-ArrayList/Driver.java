cd3c75c5190d0efdf0cb318b6ff3c1bab4f621b0
Jason Shin <jasonshin98@gmail.com>
2014-11-12 19:00:04 -0500

    Finished 16-ArrayList

--- 16-ArrayList/Driver.java
+++ 16-ArrayList/Driver.java
@@ -6,7 +6,18 @@ public class Driver{
 	ArrayList<Integer> al = new ArrayList<Integer>();
 	Random r = new Random();
 	for (int i = 0; i < 20; i++) {
-	    ai.add(r.nextInt(5));
+	    al.add(r.nextInt(6));
 	}
-	for (int i = 0; i < al.size(); i++){
+	System.out.println("Before removing duplicates:\n" + al);
+	int i = 0;
+	while (i < al.size() - 1) {
+	    if (al.get(i) == al.get(i + 1)) {
+		al.remove(i + 1);
+	    } else {
+		i++;
+	    }
+	}
+	System.out.println("After removing duplicates:\n" + al);
+    }
+}
 	    
