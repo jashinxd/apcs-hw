ecdc4a4d451c38ef2ae9552931b4e6ad7612c555
Jason Shin <jasonshin98@gmail.com>
2014-12-01 08:41:32 -0500

    Worked on OrderedSuperArray.java

--- 19-OrderedSuperArray/OrderedSuperArray.java
+++ 19-OrderedSuperArray/OrderedSuperArray.java
@@ -2,21 +2,21 @@ public class OrderedSuperArray extends Sarray{
     
     public OrderedSuperArray(int i){
 	super(i);
-	String[] d = super.getData();
-	System.out.println(super.getData());
+	String[] d = getData();
+	System.out.println(getData());
     }
 
     /*
     public boolean add(String s) {
 	if (d[d.length - 1] != null) {
-	    super.add1index();
+	    add1index();
 	}
 	for (int i = 0; i < d.length; i++) {
 	    if (s.compareTo(d[i]) > 0) {
 		for (int ind = d.length - 1; ind > i; ind--) {
-		    super.set(d[ind + 1], ind);
+		    set(d[ind + 1], ind);
 		}
-		super.set(s, i);
+		set(s, i);
 	    }
 	}
 	return true;
