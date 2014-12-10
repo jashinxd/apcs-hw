eabcdbaff7ed5a326a465367b515e8a2d85dab16
Jason Shin <jasonshin98@gmail.com>
2014-12-10 08:46:39 -0500

    back to norm

--- 12-Stuyablo/Warrior.java
+++ 12-Stuyablo/Warrior.java
@@ -1,4 +1,4 @@
-public class Warrior extends basechar implements Comparable{
+public class Warrior extends basechar{
     public Warrior(String name){
         super.setarmor(20);
         super.setname(name);
@@ -16,17 +16,4 @@ public class Warrior extends basechar implements Comparable{
     public String getname(){
         return super.getname();
     }
-    
-    public int compareTo(Object other) {
-	Warrior o = (Warrior) other;
-	return getname().compareTo(o.getname());
-    }
-    
-    public static void main(String[] args) {
-	Warrior w1 = new Warrior("Jason");
-	Warrior w2 = new Warrior("Shin");
-	System.out.println(w1.compareTo(w2));
-	
-    }
-	
 }
