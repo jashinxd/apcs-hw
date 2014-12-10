39dfa2a333108222ed8afb2b025fcd96afc7e1f9
Jason Shin <jasonshin98@gmail.com>
2014-12-10 08:34:38 -0500

    Finished Interval Array Sorting

--- 21-Interval/Interval.java
+++ 21-Interval/Interval.java
@@ -1,21 +1,25 @@
 import java.util.*;
 import java.io.*;
 
-public class Interval {
+public class Interval implements Comparable {
 
     private int low,high;
-    private Random rnd = new Random();
+    private static Random rnd = new Random();
+    private static int numIntervals = 0;
     
     public Interval(int l, int h) {
 	low = l;
 	high = h;
+	numIntervals = numIntervals + 1;
     }
     
     public Interval() {
 	low = rnd.nextInt(10);
         high = low + 1 + rnd.nextInt(10);
+	numIntervals = numIntervals + 1;
     }
-    
+
+    /*
     public int getLow() {
 	return low;
     }
@@ -23,17 +27,22 @@ public class Interval {
     public int getHigh() {
 	return high;
     }
+    */
 
     /* Returns positive value when Interval is greater, negative when Interval
        is smaller. (Basically Interval - other interval)
        Compares low value first.If low values are the same, compares high value.
     */
-    public int compareTo(Interval other) {
+    public int compareTo(Object other) {
+	/* Cast other to the appropriate type and store in a
+	   local variable for convenience.
+	*/
+	Interval o = (Interval) other;
     	int dif = 0;
-	if (getLow() == other.getLow()) {
-	    dif = getHigh() - other.getHigh();
+	if (this.low == o.low) {
+	    dif = this.high - o.high;
 	} else {
-	    dif = getLow() - other.getLow();
+	    dif = this.low - o.low;
 	}
 	return dif;
     }
@@ -61,5 +70,10 @@ public class Interval {
 	i2 = new Interval(5,1);
 	System.out.println("i1 = [5,7], i2 = [5,1]");
 	System.out.println("i1.compareTp(i2): " + i1.compareTo(i2));
+
+	// --Sortring with Interface Comparable
+	Arrays.sort(a);
+	System.out.println("\nAfter Sorting Intervals");
+	System.out.println(Arrays.toString(a));
     }
 }
