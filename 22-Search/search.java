73023537dd6b421c07560b9b43cadd2b4928a9dc
Jason Shin <jasonshin98@gmail.com>
2014-12-11 08:41:50 -0500

    Added search.java

---
+++ 22-Search/search.java
@@ -0,0 +1,14 @@
+public class search {
+
+    private 
+
+    public search(Comparable c) {
+	
+    }
+
+    public Comparable lsearch(Comparable c) {
+	for (int i = 0; i <  
+    
+    public static void main(String[] args) {
+	int Array[] 
+    
