345d89bd02b10e9052b36e6f7399c93bf17e2043
Jason Shin <jasonshin98@gmail.com>
2014-12-17 08:37:05 -0500

    gui1

--- 23-Gui/Gui1.java
+++ 23-Gui/Gui1.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 //Look up Java Layout Manager and Java Swing Components to learn
 //more about GUIs
@@ -7,10 +8,40 @@ import java.awt.*;
 public class Gui1 extends JFrame implements ActionListener{
 
     private Container pane;
-    private JButton b1,b2;
+    private JButton b1,b2,labler;
     private JLabel l;
     private JTextArea text;
     private JPanel canvas;
+    private JCheckBox box;
+    private int x = 10;
+    private int y = 10;
+
+    private class Canvas extends JPanel {
+	public void paintComponent(Graphics g){
+	    super.paintComponent(g);
+	    g.setColor(Color.red);
+	    g.fillOval(x,y,30,30);
+	}
+    }
+
+    private class Key implements KeyListener {
+	public void keyPressed (KeyEvent e){}
+	public void keyReleased (KeyEvent e){}
+	public void keyTyped (KeyEvent e){}
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	if (e.getSource() == b1) {
+	    System.out.println("You Clicked Click Me!");
+	    System.out.println("By the way, the text box is: " + text.getText());
+	    
+	} else if (e.getSource() == b2) {
+	    System.out.println("Shutting Down");
+	    System.exit(0);
+	} else if (e.getSource() == labler) {
+	    System.out.println();
+	}
+    }
     
     public Gui1(){
 
@@ -30,16 +61,26 @@ public class Gui1 extends JFrame implements ActionListener{
 	pane.add(l);
 
 	// Adding Buttons
-	b1 = new JButton("button");
+	b1 = new JButton("Click Me");
+	b1.addActionListener(this);
 	pane.add(b1);
-	b2 = new JButton("exit");
+	b2 = new JButton("Exit");
+	b2.addActionListener(this);
 	pane.add(b2);
+	labler = new JButton("Labler");
+	labler.addActionListener(this);
+	pane.add(labler);
+	box = new JCheckBox ("box");
+	box.addActionListener(this);
+	pane.add(box);
+	
 
 	// Adding Textbox
 	text = new JTextArea("HELLO");
 	text.setColumns(40);
 	text.setRows(10);
 	text.setBorder(BorderFactory.createLineBorder(Color.red, 2));
+	text.addKeyListener(new Key());
 	pane.add(text);
 
 	//Adding new Panel to Main Panel
